9ac5967310df391cb40536a309cfa5904b0217f4
IsaacGomez10 <isaac.21.10@hotmail.com>
2021-08-17 09:43:35 -0500

    Producto, vistas, comando delete de confirmar y detalles

--- Asp/Controllers/ProductoController.cs
+++ Asp/Controllers/ProductoController.cs
@@ -62,5 +62,81 @@ namespace Asp.Controllers
                 return View();
             }
         }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    producto findProduct = db.producto.Where(a => a.id == id).FirstOrDefault();
+                    return View(findProduct);
+                }
+            }
+            catch(Exception ex)
+            {
+
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findProduct = db.producto.Find(id);
+                return View(findProduct);
+            }
+        }
+
+         [HttpPost]
+         [ValidateAntiForgeryToken]
+
+         public ActionResult Edit(producto editProduct)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    producto product = db.producto.Find(editProduct.id);
+
+                    product.nombre = editProduct.nombre;
+                    product.percio_unitario = editProduct.percio_unitario;
+                    product.descripcion = editProduct.descripcion;
+                    product.cantidad = editProduct.cantidad;
+                    product.id_proveedor = editProduct.id_proveedor;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    producto product = db.producto.Find(id);
+                    db.producto.Remove(product);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "Error" + ex);
+                return View();
+            }
+        }
     }
 }
