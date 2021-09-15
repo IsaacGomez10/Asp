1b0a7b7a614962e0ecbc65276ae8fd757f0212ed
IsaacGomez10 <isaac.21.10@hotmail.com>
2021-09-14 23:15:27 -0500

    upload CSV para proveedor cion error proximo a arreglar.

--- Asp/Controllers/ProveedorController.cs
+++ Asp/Controllers/ProveedorController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Asp.Models;
+using System.IO;
 
 namespace Asp.Controllers
 {
@@ -25,7 +26,6 @@ namespace Asp.Controllers
             return View();
         }
 
-        [HttpPost]
         [ValidateAntiForgeryToken]
 
         public ActionResult Create(proveedor provider)
@@ -122,5 +122,74 @@ namespace Asp.Controllers
                 return View();
             }
         }
+
+        public ActionResult uploadCSV()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult uploadCSV(HttpPostedFileBase fileForm)
+        {
+            try
+            {
+                //string para guardar la ruta
+                string filePath = string.Empty;
+
+                //condicion para saber si el archivo llego
+                if (fileForm != null)
+                {
+                    //ruta de la carpeta que guardara el archivo
+                    string path = Server.MapPath("~/Uploads/");
+
+                    //condicion para saber si la carpeta uploads existe
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    //obtener nombre del archivo
+                    filePath = path + Path.GetFileName(fileForm.FileName);
+
+                    //obtener la extencion del archivo
+                    string extension = Path.GetExtension(fileForm.FileName);
+
+                    //guardar el archivo
+                    fileForm.SaveAs(filePath);
+
+                    string csvData = System.IO.File.ReadAllText(filePath);
+
+                    foreach (string row in csvData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            var newProveedor = new proveedor
+                            {
+                                nombre = row.Split(';')[0],
+                                direccion = row.Split(';')[1],
+                                telefono = row.Split(';')[2],
+                                nombre_contacto = row.Split(';')[3]
+                            };
+
+                            using (var db = new inventario2021Entities())
+                            {
+                                db.proveedor.Add(newProveedor);
+                                db.SaveChanges();
+                            }
+                        }
+                    }
+                }
+
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error " + ex);
+                return View();
+            }
+        }
+
     }
 }
