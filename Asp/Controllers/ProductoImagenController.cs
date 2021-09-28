6f38a9966b57cb37d334db05cbaae2b454f2b422
IsaacGomez10 <isaac.21.10@hotmail.com>
2021-09-28 14:58:43 -0500

    Producto imagen

--- Asp/Controllers/ProductoImagenController.cs
+++ Asp/Controllers/ProductoImagenController.cs
@@ -5,21 +5,26 @@ using System.Web;
 using System.Web.Mvc;
 using Asp.Models;
 using System.IO;
+using System.Web.Routing;
 
-namespace Asp.Controllers
+namespace ProyectoASPTrimestre3.Controllers
 {
-    public class ProductoImagenController : Controller
+    public class Producto_ImagenController : Controller
     {
-        // GET: ProductoImagen
+        // GET: Producto_Imagen
         public ActionResult Index()
-        {
-            return View();
-        }
-        public static string NombreProducto(int idProduct)
         {
             using (var db = new inventario2021Entities())
             {
-                return db.producto.Find(idProduct).nombre;
+                return View(db.producto_imagen.ToList());
+            }
+        }
+
+        public static string nombreProducto(int idProducto)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.producto.Find(idProducto).nombre;
             }
         }
 
@@ -38,22 +43,21 @@ namespace Asp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-
         public ActionResult CargarImagen(int id_producto, HttpPostedFileBase imagen)
         {
-
             try
             {
+                //string para guardar la ruta
                 string filePath = string.Empty;
                 string nameFile = "";
 
-
+                //condicion para saber si el archivo llego
                 if (imagen != null)
                 {
+                    //ruta de la carpeta que guardara el archivo
+                    string path = Server.MapPath("~/UploadsImagenes");
 
-                    string path = Server.MapPath("~/UploadsPictures");
-
-
+                    //condicion para saber si la carpeta uploads existe
                     if (!Directory.Exists(path))
                     {
                         Directory.CreateDirectory(path);
@@ -61,32 +65,35 @@ namespace Asp.Controllers
 
                     nameFile = Path.GetFileName(imagen.FileName);
 
-
+                    //obtener el nombre del archivo
                     filePath = path + Path.GetFileName(imagen.FileName);
 
-
+                    //obtener la extension del archivo
                     string extension = Path.GetExtension(imagen.FileName);
 
-
+                    //guardar el archivo
                     imagen.SaveAs(filePath);
-
-                    using (var db = new inventario2021Entities())
-                    {
-                        var imagenProducto = new producto_imagen();
-                        imagenProducto.id_producto = id_producto;
-                        imagenProducto.imagen = "/UploadsPictures" + nameFile;
-                        db.producto_imagen.Add(imagenProducto);
-                        db.SaveChanges();
-                    }
                 }
-                return View();
-            }
 
+                using (var db = new inventario2021Entities())
+                {
+                    var imagenProducto = new producto_imagen();
+                    imagenProducto.id_producto = id_producto;
+                    imagenProducto.imagen = "/UploadsImagenes" + nameFile;
+                    db.producto_imagen.Add(imagenProducto);
+                    db.SaveChanges();
+
+                }
+
+                return View();
+
+            }
             catch (Exception ex)
             {
-                ModelState.AddModelError("", "Error " + ex);
+                ModelState.AddModelError("", "error " + ex);
                 return View();
             }
+
         }
+
     }
-}
